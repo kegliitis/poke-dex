9bdd188fa68134c1ca77b433e4aee7c24bbe43f1
kegliitis <eglitiskristians@gmail.com>
2023-04-23 13:16:37 +0200

    Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about-container">
-      <h1>About Us</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus semper est nec euismod. Fusce tincidunt quis mauris non bibendum. Etiam non leo non sem maximus fringilla. Fusce vel ipsum et libero blandit tincidunt at at arcu. Aliquam eget facilisis odio, quis hendrerit mi. Praesent vel justo ut nisi interdum commodo eget sed lectus. Duis ut mi augue.</p>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed fermentum eleifend sem sit amet bibendum. Duis eget odio vitae justo tincidunt bibendum. Nam euismod auctor mauris, vitae consequat sapien malesuada ut. Aliquam ut mi quis ipsum faucibus blandit ut a odio. Aliquam eget enim quis sapien malesuada facilisis. Duis quis felis ut quam commodo ultrices. Sed iaculis justo nunc, at posuere elit aliquet eu. </p>
+      <h1>Requirements</h1>
+      <br></br>
+      <ul>
+        <li>Pokémon information must be retrieved from PokéAPI. &#10004;</li>
+        <li>The Pokédex must display a list of Pokémon and support simple pagination (e.g. “next” and “previous” buttons to get the next/previous page of Pokémon), such that not all Pokémon are displayed at the same time. &#10004;</li>
+        <li>When a user clicks on a specific Pokémon, additional information about that Pokémon must be displayed. E.g. type(s), stats, abilities, height and weight. &#10004;</li>
+        <li>The application must contain multiple pages (e.g. "pokedex" and "about") and utilize React Router to route between them. &#10004;</li>
+        <li>The application must be developed using the Create React App toolchain. &#10004;</li>
+      </ul>
+      <br></br>
+      <p>Assignment made by Kristians</p>
     </div>
   );
 };
